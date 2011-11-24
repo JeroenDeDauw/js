6e9dded8002cd9583e8284076b4df53ef7c057d9
Jeroen De Dauw <jeroendedauw@gmail.com>
2011-11-24 00:53:37 +0000

    adding etherpad name steal script

---
+++ etherpad.js
@@ -0,0 +1,30 @@
+/**
+ * Simple repeating script to paste into your browsers console window
+ * when joininh an etherpad. It will fill in names for people as soon
+ * as they join.
+ * 
+ * @licence GNU GPL v3+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+(function () { 
+	var _this = this;
+	this.count = 0;
+	
+	this.getName = function() {
+		return "I'm to slow to fill in my name!" + ( this.count === 0 ? '' : this.count );
+	};
+	
+	this.fillInEmpties = function() {
+		e = jQuery.Event("keypress");
+		e.which = 13;
+		
+		$( '.editempty' ).val( this.getName() ).trigger( e );
+	}
+	
+	this.doLoop = function() {
+		this.fillInEmpties();
+		setTimeout( function() { _this.doLoop(); }, 500 );
+	};
+	
+	this.doLoop();
+})();
